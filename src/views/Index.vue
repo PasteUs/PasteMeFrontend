faf867ca5f2508e4cb2b22b33e71f545cf248692
Lucien <lucien@lucien.ink>
2019-08-25 18:14:29 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -38,7 +38,6 @@
                     this.api.get(this.$store.getters.config.api + this.$route.params.key, {
                         json: true
                     }).then(response => {
-                        alert(JSON.stringify(response));
                         if (response.status === 200) {
                             this.updateView("paste_view");
                             this.updateContent(response.content);
