8830db2e0ec7ec8ffb7d82ff6e7ad0ec86e37c3d
Lucien Shui <lucien@lucien.ink>
2019-08-31 02:44:56 +0800

    Fix

--- shell/daily.sh
+++ shell/daily.sh
@@ -10,4 +10,4 @@ git config user.email "lucien@lucien.ink" && \
 git add --all && \
 git commit -m "build from travis-ci $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')" && \
 set +x && \
-git push https://${GH_TOKEN}@github.com/LucienShui/PasteMeFrontend.git master
+git push https://${GH_TOKEN}@github.com/LucienShui/PasteMeFrontend.git dev
