b20319fcd7c19469da88b073a40aa51a3d54b395
Lucien Shui <lucien@lucien.ink>
2019-08-31 03:28:47 +0800

    Fix merge

--- shell/daily.sh
+++ shell/daily.sh
@@ -5,10 +5,10 @@ git clone https://github.com/LucienShui/PasteMeFrontend.git -b dev frontend && \
 cd frontend || exit 1 && \
 git config user.name "Lucien Shui" && \
 git config user.email "lucien@lucien.ink" && \
-git merge daily && \
-git clone https://github.com/PasteUs/CDN.git -b master --depth=1 pasteme_cdn
+git merge origin/daily && \
+git clone https://github.com/PasteUs/CDN.git -b master --depth=1 pasteme_cdn && \
 echo \{\"version\": \"$(cat pasteme_cdn/version.txt)\"\} > cdn.version.json && \
-rm -rf pasteme_cdn
+rm -rf pasteme_cdn && \
 git add --all && \
 git commit -m "build from travis-ci $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')" && \
 set +x && \
