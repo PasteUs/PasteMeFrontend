d225cead29910aed16e88a80268a82fc67bfdbc7
Lucien Shui <lucien@lucien.ink>
2019-09-07 04:27:26 +0800

    Update travis.yml

--- shell/dev.sh
+++ shell/dev.sh
@@ -17,8 +17,9 @@ set -x && \
 cd .. && \
 git clone https://github.com/LucienShui/PasteMeFrontend.git -b dist/"${TRAVIS_BRANCH}" tmpdir && \
 cp -r tmpdir/.git pasteme && \
-cp LICENSE DEPLOY.md Dockerfile pasteme && \
+cp LICENSE DEPLOY.md dev.Dockerfile pasteme && \
 cd pasteme && \
+mv dev.Dockerfile Dockerfile && \
 rm report.html && \
 mv DEPLOY.md README.md && \
 git config user.name "Lucien Shui" && \
@@ -26,6 +27,4 @@ git config user.email "lucien@lucien.ink" && \
 git add --all && \
 git commit -m "travis-ci $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')" && \
 set +x && \
-git push https://"${GH_TOKEN}"@github.com/LucienShui/PasteMeFrontend.git dist/"${TRAVIS_BRANCH}" && \
-curl -X POST "${WEBHOOK}""${WEBHOOK_PATH}" && \
-bash shell/pasteme.dev.update.sh && \
+git push https://"${GH_TOKEN}"@github.com/LucienShui/PasteMeFrontend.git dist/"${TRAVIS_BRANCH}"
