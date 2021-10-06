86011f4e91893e7c1f8925fb6e60f1d344512187
Lucien Shui <lucien@lucien.ink>
2021-10-07 01:35:24 +0800

    add wrapper in api, optimer error display

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -4,19 +4,40 @@ function getLast(value) {
     return value[value.length - 1];
 }
 
+/**
+ * 判断是否属于后端已定义的返回值
+ * @param error
+ * @returns boolean
+ */
+function validator(error) {
+    return error.response && error.response.data && error.response.data.code &&
+        error.response.data.message && typeof error.response.data.code === 'number' &&
+        error.response.data.code > 20000;
+}
 
-export default {
-    get: function (url, params = {}, acceptedCode = [], errorHandler = this.errorHandler) {
+function defaultErrorHandler(error, alert_error = true) {
+    if (alert_error) {
+        if (validator(error)) {
+            let data = error.response.data;
+            alert(data.code + ': ' + data.message);
+        } else {
+            alert(JSON.stringify({
+                message: error.message,
+                method: error.config.method,
+                url: error.config.url,
+                params: error.config.params
+            }))
+        }
+    }
+}
+
+function wrapper(func) {
+    return function (url, params = {}, acceptedCode = [], errorHandler = defaultErrorHandler) {
         return new Promise((resolve, reject) => {
-            axios.get(url, {
-                params: params,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            }).then(response => {
+            func(url, params).then(response => {
                 resolve(response.data);
             }).catch(error => {
-                if (this.validator(error)) {
+                if (validator(error)) {
                     let data = error.response.data;
                     if (acceptedCode.includes(data.code)) {
                         resolve(data)
@@ -27,54 +48,25 @@ export default {
                 reject(error);
             });
         });
-    },
-    post: function (url, params = {}) {
-        return new Promise((resolve, reject) => {
-            axios.post(url, params).then(response => {
-                resolve(response.data);
-            }).catch(error => {
-                this.errorHandler(error);
-                reject(error);
-            });
-        });
-    },
-    put: function (url, params = {}) {
-        return new Promise((resolve, reject) => {
-            axios.put(url, params).then(response => {
-                resolve(response.data);
-            }).catch(error => {
-                this.errorHandler(error);
-                reject(error);
-            });
-        });
-    },
+    }
+}
+
+export default {
+    get: wrapper(function (url, params) {
+        return axios.get(url, {
+            params: params,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+    }),
+    post: wrapper(axios.post),
+    put: wrapper(axios.put),
+    delete: wrapper(axios.delete),
+    patch: wrapper(axios.patch),
     join: function (...args) {
         let result = args.map(pathPart => pathPart.replace(/(^\/|\/$)/g, "")).join("/");
         return result + (getLast(getLast(args)) === '/' ? '/' : '');
     },
-    /**
-     * 判断是否属于后端已定义的返回值
-     * @param error
-     * @returns boolean
-     */
-    validator: function (error) {
-        return error.response && error.response.data && error.response.data.code &&
-            error.response.data.message && typeof error.response.data.code === 'number' &&
-            error.response.data.code > 20000;
 
-    },
-    errorHandler: function (error, alert_error = true) {
-        if (alert_error) {
-            if (this.validator(error)) {
-                alert(JSON.stringify(error.response.data))
-            } else {
-                alert(JSON.stringify({
-                    message: error.message,
-                    method: error.config.method,
-                    url: error.config.url,
-                    params: error.config.params
-                }))
-            }
-        }
-    }
 }
